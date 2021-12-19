1e9d311377c2e898c32dc9693362437cc2cc8783
nguyễn văn tính <nguyenvantinh1704@gmail.com>
2021-12-20 01:09:35 +0700

    nhập hàng

---
+++ WebBanHang/Controllers/QuanLyPhieuNhapController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHang.Models;
+
+namespace WebBanHang.Controllers
+{
+    public class QuanLyPhieuNhapController : Controller
+    {
+        SellPhoneContext db = new SellPhoneContext();
+        // GET: QuanLyPhieuNhap
+        public ActionResult NhapHang()
+        {
+            ViewBag.MaNCC = db.NhaCungCaps;
+            ViewBag.ListSanPham = db.SanPhams;
+            return View();
+        }
+    }
+}
